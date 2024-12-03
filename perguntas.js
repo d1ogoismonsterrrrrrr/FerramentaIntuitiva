c8978dadffac0f1910a355788b3dc1448284bc42
d1ogoismonsterrrrrrr <0000112697013xsp@al.educacao.sp.gov.br>
2024-12-03 17:07:56 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,31 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz mulher em Inglês?',
+    'Mulher em inglês é Woman'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz teclado em Inglês?',
+    'Teclado em inglês é Keyboard'
+)
